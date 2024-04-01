496dc2b8d23e698ab6154c66fb21cef603c507b4
jvvassao <132643815+jvvassao@users.noreply.github.com>
2024-04-01 02:35:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUptade(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change',handleUptade));
+inputs.forEach(input => input.addEventListener('mousemove',handleUptade));
